353ac95fb136e84565c96026f475807764c69259
simonsdz <siemens.balg@gmail.com>
2022-06-11 14:54:32 +0530

    breaking the unit test

--- src/test/java/JavaLibrary/LibraryTest.java
+++ src/test/java/JavaLibrary/LibraryTest.java
@@ -9,6 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'")
     }
 }
